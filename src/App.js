d74b11b15b7d90ada143f37d513c80bfb2d162c5
AayushN01 <aayushniraula1@gmail.com>
2024-05-04 08:35:57 +0545

    Initial Commit: Setup Router + Functional Component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HeaderComponent from './components/HeaderComponent';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './styles/header.css';
+
+const HeaderComponent = () => {
+    return 
+    <div className='header'>
+        <h1>This is a header component</h1>
+    </div>;
+}
+
+export default HeaderComponent;
